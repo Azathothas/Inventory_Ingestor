0964d5c0c9d61cc7276eaa1197469e8c26eba33d
Ajam <AjamX101@gmail.com>
2023-08-31 13:35:09 +0545

    Update puredns_setup_gitpod.sh

--- .github/scripts/puredns_setup_gitpod.sh
+++ .github/scripts/puredns_setup_gitpod.sh
@@ -3,7 +3,8 @@
 # For debug
 #set -x
 #Usage
-# Make sure to have a new tmux session running: tmux new-session -s "puredns"
+# Make sure to have a new tmux session running: 
+# sudo apt install tmux -y ; tmux new-session -s "puredns"
 # bash <(cat $GITPOD_REPO_ROOT/.github/scripts/puredns_setup_gitpod.sh)
 # bash <(https://raw.githubusercontent.com/Azathothas/Inventory_Ingestor/main/.github/scripts/puredns_setup_gitpod.sh)
 #----------------------------------------------------------------------------#
