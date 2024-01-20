0e6429a3ac02ed85f6c25fa2bfa9443d7416a512
Azathothas <AjamX101@gmail.com>
2024-01-20 11:41:44 +0545

    +

--- .github/scripts/puredns_setup_gitpod.sh
+++ .github/scripts/puredns_setup_gitpod.sh
@@ -21,37 +21,37 @@ pip install apprise
 pip install archey4
 #-------------------------#
 #eget (For Binaries)
-sudo curl -qfsSL "https://bin.prashansa.com.np/x86_64_Linux/eget" -o "/usr/local/bin/eget" && sudo chmod +xwr "/usr/local/bin/eget"
+sudo curl -qfsSL "https://bin.ajam.dev/x86_64_Linux/eget" -o "/usr/local/bin/eget" && sudo chmod +xwr "/usr/local/bin/eget"
 # 7z (Archive)
 sudo rm /usr/bin/7z 2>/dev/null ; sudo rm /usr/local/bin/7z 2>/dev/null
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/7z" --to "/usr/local/bin/7z"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/7z" --to "/usr/local/bin/7z"
 7z -h
 #anew (tee -a)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/anew" --to "/usr/local/bin/anew"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/anew" --to "/usr/local/bin/anew"
 anew -h
 #btop (For Observability & Stats)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/btop" --to "/usr/local/bin/btop"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/btop" --to "/usr/local/bin/btop"
 btop -h
 #croc
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/croc" --to "/usr/local/bin/croc"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/croc" --to "/usr/local/bin/croc"
 croc -h
 #inscope (For Scope)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/inscope" --to "/usr/local/bin/inscope"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/inscope" --to "/usr/local/bin/inscope"
 inscope -h
 #massdns (For PureDNS|ShuffleDNS)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/massdns" --to "/usr/local/bin/massdns"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/massdns" --to "/usr/local/bin/massdns"
 massdns -h
 #resDNS (Main, should auto install other lacking deps)
 sudo eget "https://raw.githubusercontent.com/Azathothas/Arsenal/main/resdns/resdns.sh" --to "/usr/local/bin/resdns" ; sudo chmod +xwr "/usr/local/bin/resdns"
 resdns -h
 #scopeview (For Scope)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/scopeview" --to "/usr/local/bin/scopeview"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/scopeview" --to "/usr/local/bin/scopeview"
 scopeview -h
 #shuffledns (Not Really Needed)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/shuffledns" --to "/usr/local/bin/shuffledns"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/shuffledns" --to "/usr/local/bin/shuffledns"
 shuffledns -h
 #yq (For Yaml)
-sudo eget "https://bin.prashansa.com.np/x86_64_Linux/yq" --to "/usr/local/bin/yq"
+sudo eget "https://bin.ajam.dev/x86_64_Linux/yq" --to "/usr/local/bin/yq"
 yq -h
 #----------------------------------------------------------------------------#
 
